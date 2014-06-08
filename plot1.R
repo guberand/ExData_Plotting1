75fc2184632ee8f245cf5de535700fa8a6402cda
guberand <guberand@gmail.com>
2014-06-08 18:13:14 -0400

    Update plot1.R
    plot1.R updated

--- plot1.R
+++ plot1.R
@@ -3,9 +3,8 @@
 ##Reading the txt file and tranforming it prior to plotting
   electric<-read.table("household_power_consumption.txt",header=T,sep=";",stringsAsFactors=F)
 electric1<-electric[complete.cases(electric),]
-##Faced issue narrowing down to date as 02/01/2007 was not working, worked when 2/1/2007 is used instead
 electric2<-subset(electric1,Date=="1/2/2007" | Date=="2/2/2007",row.names=NULL)
-electric2$Date1<-paste(electric2$Date,electric2$Time,sep=" ")#combine Date and time to a single column
+electric2$Date1<-paste(electric2$Date,electric2$Time,sep=" ")#combining Date and time to a single column
 electric2$Date1<-strptime(electric2$Date1,"%d/%m/%Y %H:%M:%S")# convert Date/time from character to Date-Time
 
 ##plot1
